35f46a55daa2d8f241c5caee923a2047f2feb68b
JoaoLuizBorges <joaoluizosborges@gmail.com>
2023-07-18 16:54:14 -0300

    Sexto Commit

--- python/modules/Deye.py
+++ python/modules/Deye.py
@@ -9,6 +9,7 @@ def Monit_Deye():
 
     usuario = "AZTEC"
     senha = "@Zt3cenergia"
+    scroll_time = 1
 
     global web
 
@@ -36,63 +37,74 @@ def Monit_Deye():
     options.add_argument('--ignore-certificate-errors')
 
     web = webdriver.Chrome(options=options)
+    try:
+        web.get("https://pro.solarmanpv.com/login")
 
-    web.get("https://pro.solarmanpv.com/login")
+        WebDriverWait(web,10).until(EC.element_to_be_clickable((By.XPATH,'//button[text()="Continue a usar o navegador atual"]'))).click()
+        time.sleep(1)
 
-    WebDriverWait(web,10).until(EC.element_to_be_clickable((By.XPATH,'//button[text()="Continue a usar o navegador atual"]'))).click()
-    time.sleep(1)
+        WebDriverWait(web,10).until(EC.element_to_be_clickable((By. XPATH, '//*[@id="app"]/div[3]/div[2]/div/div[3]/button[1]'))).click()
 
-    WebDriverWait(web,10).until(EC.element_to_be_clickable((By. XPATH, '//*[@id="app"]/div[3]/div[2]/div/div[3]/button[1]'))).click()
+        time.sleep(3.5)
 
-    time.sleep(3.5)
+        elemento = web.find_element(By.XPATH, '//*[@id="areaList"]/div[1]/button[6]')
+        web.execute_script('return arguments[0].scrollIntoView();', elemento)
+        web.execute_script('arguments[0].click();', elemento)
 
-    elemento = web.find_element(By.XPATH, '//*[@id="areaList"]/div[1]/button[6]')
-    web.execute_script('return arguments[0].scrollIntoView();', elemento)
-    web.execute_script('arguments[0].click();', elemento)
+        if web.find_element(By. CSS_SELECTOR, '#app > div.frame-login > div.loginHead.pl6x.pr6x.fsLv2.cb.vaM_E > div > div > div:nth-child(1) > div > div > div.toolBar.taR.bdSplitLine-top.vaM_E > button') == True:
+            WebDriverWait(web,5).until(EC.element_to_be_clickable((By. CSS_SELECTOR, '#app > div.frame-login > div.loginHead.pl6x.pr6x.fsLv2.cb.vaM_E > div > div > div:nth-child(1) > div > div > div.toolBar.taR.bdSplitLine-top.vaM_E > button'))).click()
 
-    if web.find_element(By. CSS_SELECTOR, '#app > div.frame-login > div.loginHead.pl6x.pr6x.fsLv2.cb.vaM_E > div > div > div:nth-child(1) > div > div > div.toolBar.taR.bdSplitLine-top.vaM_E > button') == True:
-        WebDriverWait(web,50).until(EC.element_to_be_clickable((By. CSS_SELECTOR, '#app > div.frame-login > div.loginHead.pl6x.pr6x.fsLv2.cb.vaM_E > div > div > div:nth-child(1) > div > div > div.toolBar.taR.bdSplitLine-top.vaM_E > button'))).click()
+        time.sleep(6)
 
-    time.sleep(6)
+        WebDriverWait(web,50).until(EC.element_to_be_clickable((By. XPATH, '//*[@id="app"]/div[3]/div[5]/div/div/div[1]/div/div/div[3]/button'))).click()
+        time.sleep(2.5)
 
-    WebDriverWait(web,50).until(EC.element_to_be_clickable((By. XPATH, '//*[@id="app"]/div[3]/div[5]/div/div/div[1]/div/div/div[3]/button'))).click()
-    time.sleep(2.5)
+        WebDriverWait(web,50).until(EC.element_to_be_clickable((By. XPATH, '//*[@id="app"]/div[3]/div[6]/section/div[2]/div[2]/div[1]/div/div[3]/span'))).click()
+        print("Login")
+        time.sleep(3)
 
-    WebDriverWait(web,50).until(EC.element_to_be_clickable((By. XPATH, '//*[@id="app"]/div[3]/div[6]/section/div[2]/div[2]/div[1]/div/div[3]/span'))).click()
-    print("Login")
-    time.sleep(3)
+        WebDriverWait(web,50).until(EC.element_to_be_clickable((By. XPATH, '//*[@id="app"]/div[3]/div[6]/section/div[2]/div[2]/div[2]/div/input'))).send_keys(usuario)
+        WebDriverWait(web,50).until(EC.element_to_be_clickable((By. XPATH, '//*[@id="app"]/div[3]/div[6]/section/div[2]/div[2]/div[3]/input'))).send_keys(senha)
+        time.sleep(4)
 
-    WebDriverWait(web,50).until(EC.element_to_be_clickable((By. XPATH, '//*[@id="app"]/div[3]/div[6]/section/div[2]/div[2]/div[2]/div/input'))).send_keys(usuario)
-    WebDriverWait(web,50).until(EC.element_to_be_clickable((By. XPATH, '//*[@id="app"]/div[3]/div[6]/section/div[2]/div[2]/div[3]/input'))).send_keys(senha)
-    time.sleep(4)
+        WebDriverWait(web,50).until(EC.element_to_be_clickable((By. XPATH, '//*[@id="app"]/div[3]/div[6]/section/div[2]/div[2]/button'))).click()
+        time.sleep(39)
+        print("No monitoramento")
+        web.save_screenshot('teste_acesso.png')
 
-    WebDriverWait(web,50).until(EC.element_to_be_clickable((By. XPATH, '//*[@id="app"]/div[3]/div[6]/section/div[2]/div[2]/button'))).click()
-    time.sleep(12)
+        count = 0
+        i = 0
+        dadosMonit = []
+        nome_cliente = []
 
-    print("No monitoramento")
+        #elemento = web.find_element(By.CSS_SELECTOR, '#mainFrame > div:nth-child(5) > div.frame-main.tra-fast > section > div > div.col-12 > div > div > div:nth-child(4) > div.table.tra-fast.posR > div.umy-ui.cb.h100pct > div > div.singleTable.elx-grid.t--animat > div > div.elx-table--fixed-wrapper > div.elx-table--fixed-left-wrapper > div.elx-table--body-wrapper.fixed-left--wrapper > table > tbody > tr:nth-child(10) > td.elx-body--column.col_3.col--last.col--ellipsis > div > div > div:nth-child(2)')
+        #web.execute_script('return arguments[0].scrollIntoView();', elemento)
+        #time.sleep(18)
+        #web.save_screenshot('teste_acesso2.png')
 
-    pageSource = web.page_source
+        pageSource = web.page_source
+        pg_html = sp(pageSource,'html.parser')
+        print(pg_html.text)
 
-    pg_html = sp(pageSource,'html.parser')
-    print(pg_html.text)
+        for dados in pg_html.find_all("div",attrs={"class": "textCut"}):
+            dadosMonit.append(dados['title'])
 
-    count = 0
-    i = 0
-    dadosMonit = []
-    nome_cliente = []
+        for i in dadosMonit:
+            if count % 2 == 0:
+                nome_cliente.append(i)
+            count += 1
 
-    for dados in pg_html.find_all("div",attrs={"class": "textCut"}):
-        dadosMonit.append(dados['title'])
+        nome_cliente = set(nome_cliente)
 
-    for i in dadosMonit:
-        if count % 2 == 0:
-            nome_cliente.append(i)
-        count += 1
+        if not nome_cliente:
+            Monit_Deye()
 
-    print(nome_cliente)
+        with open('lista_clientes.txt','w',encoding='utf-8') as ls:
+            ls.writelines(nome_cliente)
 
-    with open("pg_html.txt","w", encoding='utf-8') as g:
-        g.write(pageSource)
+    except Exception as e:
+        print(str(e))
+        Monit_Deye()
 
 Monit_Deye()
 
